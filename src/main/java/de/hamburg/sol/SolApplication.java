fcbfda281aa2553fd65fd8ab1dc17a073dd83052
Ahmed Khalil <ahmed.khalil@haw-hamburg.de>
2024-10-30 18:28:15 +0100

    Initial Commit

---
+++ src/main/java/de/hamburg/sol/SolApplication.java
@@ -0,0 +1,13 @@
+package de.hamburg.sol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolApplication.class, args);
+    }
+
+}
