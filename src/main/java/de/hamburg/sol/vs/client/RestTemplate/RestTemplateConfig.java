1865d893fe919b0d05e23e285aed8459796ac533
Ahmed Khalil <ahmed.khalil@haw-hamburg.de>
2024-11-27 16:44:04 +0100

    - RestTemplate Konfiguration geupdated. Mit Timeouts

--- src/main/java/de/hamburg/sol/vs/client/RestTemplate/RestTemplateConfig.java
+++ src/main/java/de/hamburg/sol/vs/client/RestTemplate/RestTemplateConfig.java
@@ -1,14 +1,26 @@
 package de.hamburg.sol.vs.client.RestTemplate;
 
+
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.time.temporal.TemporalUnit;
 
 @Configuration
 public class RestTemplateConfig {
 
+    // RestTemplate als Bean bereitstellen
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder) {
+        return  restTemplateBuilder.
+                setConnectTimeout(Duration.ofMillis(5000))
+                .setReadTimeout(Duration.ofMillis(5000))
+                .build();
     }
+
+
 }
