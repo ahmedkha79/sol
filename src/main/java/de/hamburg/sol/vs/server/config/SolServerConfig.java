42fc29ca30f4c2cf4ea6a7b0768a6eb6c245dec3
Ahmed Khalil <ahmed.khalil@haw-hamburg.de>
2024-12-01 19:29:03 +0100

    - Scheduler hinzugefügt

--- src/main/java/de/hamburg/sol/vs/server/config/SolServerConfig.java
+++ src/main/java/de/hamburg/sol/vs/server/config/SolServerConfig.java
@@ -6,13 +6,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 
 import java.net.SocketException;
+import java.util.concurrent.ScheduledExecutorService;
 
 @Configuration
 public class SolServerConfig {
 
+    private final ScheduledExecutorService scheduler;
+    public SolServerConfig(ScheduledExecutorService scheduler) {
+        this.scheduler = scheduler;
+    }
     @Bean
     @Lazy
     public SolServer solServer() throws SocketException, IllegalAccessException {
-        return new SolServer(4);
+        return new SolServer(scheduler,4);
     }
 }
