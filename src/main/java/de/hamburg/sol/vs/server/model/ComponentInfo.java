89d065ff015cf43d4e54095f83f49066fc7f4ffb
Ahmed Khalil <ahmed.khalil@haw-hamburg.de>
2024-12-01 19:26:50 +0100

    Timer Logik mit Scheduler hinzugefügt

--- src/main/java/de/hamburg/sol/vs/server/model/ComponentInfo.java
+++ src/main/java/de/hamburg/sol/vs/server/model/ComponentInfo.java
@@ -3,8 +3,12 @@ package de.hamburg.sol.vs.server.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Setter;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Data
 public class ComponentInfo {
@@ -16,14 +20,40 @@ public class ComponentInfo {
     private int tcpPort;
     private LocalDateTime integrationPort;
     private LocalDateTime lastInteraction;
+    private final ScheduledExecutorService scheduler;
     private String status;
+    private Runnable onTimeout;
 
     public ComponentInfo(String comUUID, String ipAddress, int tcpPort){
         this.comUUID = comUUID;
         this.ipAddress = ipAddress;
         this.tcpPort = tcpPort;
+        this.scheduler = Executors.newSingleThreadScheduledExecutor();
         this.integrationPort = LocalDateTime.now();
         this.lastInteraction = LocalDateTime.now();
+    }
+
+    public void updateLastInteraction(){
+        this.lastInteraction = LocalDateTime.now();
+    }
+
+    public void startTimeout(long timeout, TimeUnit unit, Runnable onTimeout){
+        this.onTimeout = onTimeout;
+        resetTimeout(timeout, unit);
+
+    }
+
+    public void resetTimeout(long timeout, TimeUnit unit){
+
+        scheduler.schedule(() -> {
+            if(onTimeout != null){
+                onTimeout.run();
+            }
+        }, timeout, unit);
+    }
+
+    public void stopTimeout(){
+        scheduler.shutdownNow();
     }
 
 }
