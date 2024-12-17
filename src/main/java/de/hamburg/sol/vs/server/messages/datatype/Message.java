e39a1f747371a1a19f93a771b35b717337cbd31c
Ahmed Khalil <ahmed.khalil@haw-hamburg.de>
2024-12-17 23:09:51 +0100

    - Message Schnittstelle - POST Methode implementiert

---
+++ src/main/java/de/hamburg/sol/vs/server/messages/datatype/Message.java
@@ -0,0 +1,44 @@
+package de.hamburg.sol.vs.server.messages.datatype;
+
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.annotation.Nullable;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@Builder
+public class Message {
+
+    @JsonProperty("star")
+    private String star;
+
+    @JsonProperty("origin")
+    private String origin;
+
+    @JsonProperty("sender")
+    private String sender;
+
+    @Nullable
+    @JsonProperty("msg_id")
+    private String msg_id;
+
+    @JsonProperty("version")
+    private Integer version;
+
+    @JsonProperty("created")
+    private LocalDateTime created;
+
+    @JsonProperty("changed")
+    private LocalDateTime changed;
+
+    @JsonProperty("subject")
+    private String subject;
+
+    @JsonProperty("message")
+    private String message;
+}
