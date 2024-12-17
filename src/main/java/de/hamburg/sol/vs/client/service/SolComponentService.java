f9948177d28023aa4ae2600d6fe5f6139b0f5945
Ahmed Khalil <ahmed.khalil@haw-hamburg.de>
2024-12-18 00:18:15 +0100

    - handleExitCommand verschoben

--- src/main/java/de/hamburg/sol/vs/client/service/SolComponentService.java
+++ src/main/java/de/hamburg/sol/vs/client/service/SolComponentService.java
@@ -94,49 +94,6 @@ public class SolComponentService {
         }
     }
 
-    public void handleExitCommand(){
-        String url = String.format("http://%s:%d/vs/v1/system/%s?star=%s",
-                solComponent.getSolIpAddress(),
-                solComponent.getSolPort(),
-                solComponent.getComUUID(),
-                solComponent.getStarUUID()
-                );
-        int retries = 0;
-        int timeOut = 10000;
-        boolean disconnectSuccessful = false;
-
-        while(retries <= 1 && !disconnectSuccessful){
-            try {
-
-                HttpHeaders headers = new HttpHeaders();
-                headers.setAccept(List.of(MediaType.TEXT_PLAIN));
-                HttpEntity<String> entity = new HttpEntity<>(headers);
-                ResponseEntity<String> deleteRequest = restTemplate.exchange(url, HttpMethod.DELETE, entity, String.class);
-
-                if(deleteRequest.getStatusCode().is2xxSuccessful()){
-                    log.info("Erfolgreich von Sol abgemeldet");
-                    disconnectSuccessful = true;
-                } else {
-                    log.warn("Sol antwortete mit Status: {}", deleteRequest.getStatusCode());
-                    retries++;
-                    waitBeforeRetry(timeOut);
-                }
-
-
-            } catch (Exception e) {
-                log.error("Fehler beim Abmelden von Sol");
-            }
-        }
-
-        if(!disconnectSuccessful){
-            log.info("SOL konnte nicht erreicht werden, bereite Abschaltung vor...");
-        }
-
-        solComponent.terminateComponent();
-
-
-    }
-
 
     private SolProtocol createSolProtocol(){
 
