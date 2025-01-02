5433a0473c5abb5b60c6f05af6c7d1c27921e7e5
Ahmed Khalil <ahmed.khalil@haw-hamburg.de>
2025-01-02 23:05:19 +0100

    feat: - GROUP_ID kann variabel gesetzt werden       - GALAXY_PORT hinzugefügt

--- src/main/java/de/hamburg/sol/vs/config/global/GlobalConfig.java
+++ src/main/java/de/hamburg/sol/vs/config/global/GlobalConfig.java
@@ -3,28 +3,42 @@ package de.hamburg.sol.vs.config.global;
 public class GlobalConfig {
 
     private static Integer STARPORT;
+    private static Integer GROUP_ID;
+    private static Integer GALAXY_PORT;
+    private static Integer GALAXY_ID;
     private static final Integer DEFAULT_PORT = 8000;
-    private static final int GROUP_ID = 6;
+    private static final int DEFAULT_GROUP_ID = 131;
+    private static final int DEFAULT_GALAXY_ID = 200;
+
 
     static {
-        String portFromEnv = System.getenv("APP_PORT");
-        if(portFromEnv != null) {
-            try{
-                setStarPort(Integer.parseInt(portFromEnv));
-            } catch(NumberFormatException e) {
-                System.err.println("Ungültiger Wert für APP_PORT: " + portFromEnv + ". Verwende Standardport.");
-                setStarPort(DEFAULT_PORT);
-                System.err.println("Verwende Standardport.");
-            }
-        } else {
-            setStarPort(DEFAULT_PORT);
-        }
+        GROUP_ID = readEnvironmentVariable("APP_GROUP_ID", DEFAULT_GROUP_ID);
+        STARPORT = readEnvironmentVariable("APP_PORT", DEFAULT_PORT) + GROUP_ID;
+        GALAXY_ID = readEnvironmentVariable("APP_GALAXY_ID", DEFAULT_GALAXY_ID);
+        GALAXY_PORT = readEnvironmentVariable("APP_GALAXY_PORT", DEFAULT_PORT) + GALAXY_ID;  ;
+
     }
+
+    private static int readEnvironmentVariable(String envName, int defaultValue) {
+        String value = System.getenv(envName);
+        System.out.println("Environment variable " + envName + " is " + value);
+
+        if(value != null) {
+            try {
+                return Integer.parseInt(value);
+            } catch (NumberFormatException e) {
+                System.err.println("Ungültiger Wert für " + envName + ": " + value + ". Verwende Standardport.");
+            }
+        }
+        return defaultValue;
+    }
+
+
 
 
     private static int setStarPort(Integer port) {
         if(STARPORT == null) {
-            STARPORT = port + GROUP_ID;
+            STARPORT = port + DEFAULT_GROUP_ID;
         }
 
         return STARPORT;
